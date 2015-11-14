1395f394616375a8f63777ff52097344d2300d19
Nazim Amin <nazim.amin@cix.csi.cuny.edu>
2015-11-13 22:23:03 -0500

    init

---
+++ ndtndt/ndtndt/ndtndt/api.py
@@ -0,0 +1,18 @@
+﻿"""
+Routes and views for the flask application.
+"""
+
+from flask import Flask, jsonify, abort, make_response
+from flask_restful import Api, Resource, reqparse, fields, marshal
+from flask_httpauth import HTTPBasicAuth
+import pypyodbc,json,xmltodict
+
+from ndtndt import app
+
+api = Api(app)
+auth = HTTPBasicAuth()
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return jsonify({'status': 'success'})
