2f0aaee0b13ad791ce841829c0757e9b6003e991
nazim <amin.nazim13@gmail.com>
2015-11-14 01:08:12 -0500

    init

--- ndtndt/ndtndt/ndtndt/api.py
+++ ndtndt/ndtndt/ndtndt/api.py
@@ -1,18 +1,69 @@
-﻿"""
-Routes and views for the flask application.
+"""
+Routes for the flask application.
 """
 
 from flask import Flask, jsonify, abort, make_response
 from flask_restful import Api, Resource, reqparse, fields, marshal
 from flask_httpauth import HTTPBasicAuth
-import pypyodbc,json,xmltodict
+import json, xmltodict
 
-from ndtndt import app
-
+from ndtndt import app, dbconnection
 api = Api(app)
 auth = HTTPBasicAuth()
 
+# dummy, just to see that it works
 @app.route('/')
-@app.route('/home')
-def home():
-    return jsonify({'status': 'success'})
+def default():
+    try:
+        return jsonify({'init': 'api.ndtndt, call some resource uri'})
+    except Exception as e:
+        print(e)
+
+# there is no post on this
+# get returns top sellerr category
+class TopSellerCategory(Resource):
+    def get(self):
+        try:
+            dbcursor = dbconnection.cursor()
+            sqlcommand = ('select top 10 i.itemid,i.itemtype,i.yearmanufactured,count(*) as itemsold '
+                          'from auction a, item i '
+                          'where a.itemid=i.itemid and sold=1 '
+                          'group by i.itemid,i.itemtype,i.yearmanufactured '
+                          'order by itemsold desc')
+            dbcursor.execute (sqlcommand)
+            rows=dbcursor.fetchall()
+            row=str(rows)
+            row="<root>"+row[3:-4]+"</root>"
+            dbconnection.close()
+            return (json.dumps(xmltodict.parse(row),indent=4))
+        except Exception as e:
+            print(e)
+    def post(self, id):
+        pass
+
+#the post request on this item will be called when we need to post an item.
+#get with id will return the item associated with the auction[id]
+class Item(Resource):
+    def __init__(self):
+        pass
+    def post(self):
+        pass
+    def get(self, id):
+        try:
+            dbcursor = dbconnection.cursor()
+            sqlcommand =('select a.auctionid,i.itemid,a.openbid,a.bidincrement, a.currentbid, '
+                         'a.sellerid, i.itemname,i.itemtype, i.yearmanufactured, p.postdate, '
+                         'p.expiredates from auction a,item i,post p '
+                         'where i.itemid=a.itemid and p.itemid=i.itemid and a.auctionid=? for xml path')
+            myCursor.execute (SQLInsertCommand,list(id))
+            rows=myCursor.fetchall()
+            row=str(rows)
+            row="<root>"+row[3:-4]+"</root>"
+            dbconnection.close()
+            return(json.dumps(xmltodict.parse(row),indent=4))
+        except Exception as e:
+            print(e)
+
+
+api.add_resource(TopSellerCategory, '/topcategory')
+api.add_resource(Item, '/item/<string:id>')
