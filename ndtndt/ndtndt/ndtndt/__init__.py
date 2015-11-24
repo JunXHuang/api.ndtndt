1651777baea1137d97ebe3a8f248548fb1585584
antennae <junxiong.huang@stonybrook.edu>
2015-11-24 17:19:34 -0500

    added more api

--- ndtndt/ndtndt/ndtndt/__init__.py
+++ ndtndt/ndtndt/ndtndt/__init__.py
@@ -5,7 +5,6 @@ The flask application package.
 from flask import Flask
 import pypyodbc
 app = Flask(__name__)
-app = Flask(static_folder='ndtndt')
 # this is the db connection string. import it in api.py
 dbconnection = pypyodbc.connect('DRIVER={SQL Server};'
 								'Server=tcp:z6mcia7rsl.database.windows.net;'
